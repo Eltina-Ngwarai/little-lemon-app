cb9bbf2bda65303e31838f3906d8bd18ab6b4c0b
elty-annie <eltinangwarai@gmail.com>
2023-08-25 16:16:07 +0200

    created components

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <div>
+      <li>
+        <a>Contact info</a>
+      </li>
+      <li>
+        <a>Social Media Links</a>
+      </li>
+    </div>
+  );
+}
+export default Footer;
